cea5a0cf96228fa184aa8fe208a57a43b1791c13
LucHandsome <96497540+LucHandsome@users.noreply.github.com>
2024-07-30 04:10:42 +0700

    update vercel.jso

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes/index.js');
 const bodyParser = require('body-parser');
+const data = require('./Data/data.js');
 const { init } = require('./socket'); // Import hàm init từ socket.js
 
 dotenv.config();
@@ -11,31 +12,17 @@ dotenv.config();
 const app = express();
 const server = init(app); // Khởi tạo server với socket.io
 
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-// Mongoose connection
-mongoose.connect("mongodb+srv://thanhluc0606:Luc060603@cluster0.szybplt.mongodb.net/test")// đặt tên này là k dô thằng ddb đâu
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1); // Exit process with failure
-  });
-
-
-app.use(bodyParser.json());
-app.use(cors()); // Cho phép tất cả các nguồn gốc
-app.get('/',(req,res)=>{
-  res.json({message:'Hello world'})
-})
-// Định nghĩa các routes
-routes(app);
-
-// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send("Something broke!");
 });
 
-server.listen(4001, () => {
-    console.log(`Server is running on http://localhost: 3001`);
+app.use(bodyParser.json());
+app.use(cors()); // Cho phép tất cả các nguồn gốc
+routes(app);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
