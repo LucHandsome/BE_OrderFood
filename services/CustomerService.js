018f108b1cdb6b75c60cc516c5393e51fcbd8608
LucHandsome <96497540+LucHandsome@users.noreply.github.com>
2024-07-30 04:42:55 +0700

    Cập nhật bcrypt thành bcryptjs

--- services/CustomerService.js
+++ services/CustomerService.js
@@ -1,5 +1,5 @@
 const Customer = require('../models/Customer');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const signUpCustomer = async (customerData) => {
--- services/UserService.js
+++ services/UserService.js
@@ -1,5 +1,5 @@
 const User = require("../models/TaiKhoanCuaHang.js");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const { generateAccessToken, generateRefreshToken } = require("./JwtService.js");
 
 const createUser = (newUser) => {
