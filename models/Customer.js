57764ba6569a5e5c44601d4e81e8f5a5b1d524f3
LucHandsome <96497540+LucHandsome@users.noreply.github.com>
2024-09-27 01:43:16 +0700

    Cập nhật

--- models/Customer.js
+++ models/Customer.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const CustomerSchema = new mongoose.Schema(
     {
         email: { type: String, required: true, unique: true },
-        dateOfBirth: { type: Date, required: true },
-        gender: { type: String, required: true, enum: ['Nam', 'Nữ', 'Khác'] },
-        password: { type: String, required: true },
+        dateOfBirth: { type: Date, required: false },
+        gender: { type: String, required: false, enum: ['Nam', 'Nữ', 'Khác'] },
+        password: { type: String, required: false },
         customerName: { type: String, required: true },
         profileImage: { type: String, required: false }
     },
