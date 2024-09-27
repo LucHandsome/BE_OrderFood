ab9258c9f2416d34a4d9f6d0384df496dfe09aea
LucHandsome <96497540+LucHandsome@users.noreply.github.com>
2024-09-27 08:49:38 +0700

    Cập nhật

--- index.js
+++ index.js
@@ -29,22 +29,7 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!");
 });
 
-const corsOptions = {
-    origin: 'https://project-order-food.vercel.app', // Địa chỉ frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Bao gồm phương thức OPTIONS
-    allowedHeaders: ['Content-Type', 'Authorization'], // Header được phép
-    credentials: true // Nếu cần sử dụng cookie
-};
-
-app.use(cors(corsOptions));
-
-// Đoạn xử lý yêu cầu preflight (OPTIONS) cho tất cả các route
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://project-order-food.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.send(); // Kết thúc phản hồi với mã 200
-});
+app.use(cors());
 routes(app);
 
 server.listen(PORT, () => {
